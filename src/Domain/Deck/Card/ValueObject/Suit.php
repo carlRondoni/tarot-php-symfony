83bd67a5bb93ae57fbecb103a8a8a629dec3d771
carlrondoni <carlrondoni@protonmail.com>
2024-01-03 08:12:17 +0100

    few domain stuff

---
+++ src/Domain/Deck/Card/ValueObject/Suit.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TarotApp\Domain\Deck\Card\ValueObject;
+
+class Suite
+{
+
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TarotApp;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
